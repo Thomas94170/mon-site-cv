e6ec93b94a8ce6569110fe20722f577f062d360d
Thomas Thirion <thirionthomas68100@gmail.com>
2022-06-17 20:49:30 +0200

    bug _documents.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -5,22 +5,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
+          {/* <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
             crossOrigin="anonymous"
-          />
-          <script
+          /> */}
+          {/* <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossOrigin="anonymous"
-          ></script>
+          ></script> */}
 
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Hurricane&display=swap"
             rel="stylesheet"
-          ></link>
+          ></link> */}
         </Head>
         <body>
           <Main />
